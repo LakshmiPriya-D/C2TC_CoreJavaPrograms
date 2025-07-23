a99e1d4370cee8b93d42fe363e85948f99e79d27
Lakshmi Priya D <07lakshmipriya2004@gmail.com>
2025-07-23 22:58:50 +0530

    This is my First CoreJava program

---
+++ C2TC_CoreJavaPrograms/src/DayTwo/DoWhileDemo.java
@@ -0,0 +1,14 @@
+package DayTwo;
+
+public class DoWhileDemo {
+	//Program to demonstrate  do while loop
+
+			public static void main(String[] args) {
+				int i = 11;
+				do {
+					System.out.println(i);
+					i++;
+				} while (i <= 1);
+			}
+
+}
---
+++ C2TC_CoreJavaPrograms/src/DayTwo/IfElseDemo.java
@@ -0,0 +1,18 @@
+package DayTwo;
+
+public class IfElseDemo {
+	//Program to demonstrate if..else statement
+
+			public static void main(String[] args) {
+				int age=11;
+				
+				if(age>=18)
+					System.out.println(" You are eligible for Voting");	
+				else
+					System.out.println("Sorry! You need to wait more");
+
+				System.out.println("if else demonstration");
+				
+			}
+
+}
