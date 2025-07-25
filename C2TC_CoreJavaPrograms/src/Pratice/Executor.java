8cc827dbb92d1a45a73840508aedac4c3c071e9f
Lakshmi Priya D <07lakshmipriya2004@gmail.com>
2025-07-25 11:08:33 +0530

    Practice Programs

---
+++ C2TC_CoreJavaPrograms/src/Pratice/Executor.java
@@ -0,0 +1,14 @@
+package Pratice;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		Trainer t1=new Trainer(347,"Priya","Puducherry","JFS","Lead","C67");
+		System.out.println(t1);
+		
+		Student s1=new Student(347,"Priya","Puducherry",123,"JFS");
+		System.out.println(s1); 
+		
+	}
+
+}
